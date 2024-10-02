6a8fc61c0839bd43a19fe2e57766e28be7f9e4f0
Rodrigofms <rodrigoms.rec@gmail.com>
2024-10-02 09:49:27 -0300

    continuacao atividade

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,49 @@ import Body from "./components/Body";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 
+const url = "http://localhost:3000/carros";
+
 function App() {
-  const [count, setCount] = useState("");
+  const [carros, setCarros] = useState([]);
+  const [nome, setNome] = useState("");
+  const [preco, setPreco] = useState("");
+  const [modelo, setModelo] = useState("");
+  const [ano, setAno] = useState("");
+  const [editId, setEditId] = useState(null);
+
+  useEffect(() => {
+    async function fetchData(url) {
+      const resp = await fetch(url);
+      const data = await resp.json();
+      setCarros(data);
+    }
+    fetchData(url);
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const carros = {nome, preco: parseFloat(preco), ano: parseInt(ano), modelo};
+
+    let responsivo;
+  };
+  
 
   return (
     <>
       <Header />
-      <Body />
+      <Body
+        carros={carros}
+        handleEdit={handleEdit}
+        handleDelete={handleDelete}
+        preco={preco}
+        setNome={setNome}
+        setPreco={setPreco}
+        ano={ano}
+        setAno={setAno}
+        modelo={modelo}
+        setModelo={setModelo}
+        handleSubmit={handleSubmit}
+      />
       <Footer />
     </>
   );
